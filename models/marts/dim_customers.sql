b095afc8953b02f9a86de4d5a867f5673245cad1
Emanuel Palmeira <epalmeira@bi4all.pt>
2022-03-04 17:16:53 +0000

    source function test

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -3,11 +3,11 @@
 )}}
 
 with customers as(
-    select * from {{ ref('stg_customers') }}
+    select * from {{ source('stg_customers') }}
 ),
 
 orders as(
-    select * from {{ ref('stg_orders') }}
+    select * from {{ source('stg_orders') }}
 ),
 
 customer_orders as (
