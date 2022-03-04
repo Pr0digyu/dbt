73624391d6585236ea2e19468b95df176d1fe238
Emanuel Palmeira <epalmeira@bi4all.pt>
2022-03-04 17:24:09 +0000

    asd

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -3,11 +3,11 @@
 )}}
 
 with customers as(
-    select * from {{ source('stg_customers') }}
+    select * from {{ ref('stg_customers') }}
 ),
 
 orders as(
-    select * from {{ source('stg_orders') }}
+    select * from {{ ref('stg_orders') }}
 ),
 
 customer_orders as (
